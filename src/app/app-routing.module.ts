b116d7f9236ae1892f72553c2a6fa3790dd283f2
Your Name <you@example.com>
2019-02-15 18:57:33 +0100

    pok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormComponent },
+  { path: 'pokemon', component: PokemonComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
